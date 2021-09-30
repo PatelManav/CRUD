86854770bbfc08483015f3273b88bbfe8520e915
Omkar Deshmukh <omkarvd7066@gmail.com>
2021-09-30 22:34:56 +0530

    Formatted Codes

--- delete.js
+++ delete.js
@@ -13,33 +13,34 @@ application.use(bodyparser.json());
 var mysqlConnection = mysql.createConnection(details);
 
 mysqlConnection.connect((err) => {
-  if (!err) console.log("Connected Successfully!!!");
-  else console.log(err);
+    if (!err) console.log("Connected Successfully!!!");
+    else console.log(err);
 });
 
 application.listen(3003, () => {
-  console.log("server is running!!!");
+    console.log("server is running!!!");
 });
 
 application.get("/fetchinfo", (request, response) => {
-  mysqlConnection.query("SELECT * FROM Customer", (err, rows, fields) => {
-    if (!err) response.send(rows);
-    else console.log(err);
-  });
+    mysqlConnection.query("SELECT * FROM Customer", (err, rows, fields) => {
+        if (!err) response.send(rows);
+        else console.log(err);
+    });
 });
 
 application.delete("/delete", (request, response) => {
-  var id = request.body.id;
-  var message = {
-    Data: "",
-  };
-  mysqlConnection.query(
-    "DELETE FROM Customer WHERE id = ?",
-    [id],
-    (err, rows, fields) => {
-      if (!err) message["Data"] = "Deleted successfully!!!, 1 row(s) affected.";
-      else message["Data"] = "Error Occured :(";
-      response.json(message);
-    }
-  );
+    var id = request.body.id;
+    var message = {
+        Data: "",
+    };
+    mysqlConnection.query(
+        "DELETE FROM Customer WHERE id = ?",
+        [id],
+        (err, rows, fields) => {
+            if (!err)
+                message["Data"] = "Deleted successfully!!!, 1 row(s) affected.";
+            else message["Data"] = "Error Occured :(";
+            response.json(message);
+        }
+    );
 });
